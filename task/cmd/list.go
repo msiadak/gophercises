7ce06012d5ddca906d0cf80c54baf996a71be2ad
Michael Siadak <mike.siadak@gmail.com>
2018-04-20 14:20:22 -0500

    task: Implement list cmd

--- task/cmd/list.go
+++ task/cmd/list.go
@@ -2,7 +2,9 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/boltdb/bolt"
 	"github.com/spf13/cobra"
 )
 
@@ -14,6 +16,28 @@ var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "Print the list of unfinished tasks",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("1. Implement the list command")
+		db, err := bolt.Open("tasks.db", 0600, nil)
+		if err != nil {
+			fmt.Printf("Couldn't open db: '%s'\n%s\n", "tasks.db", err)
+			os.Exit(1)
+		}
+
+		err = db.View(func(tx *bolt.Tx) error {
+			b := tx.Bucket([]byte("Tasks"))
+
+			c := b.Cursor()
+
+			i := 1
+			for k, v := c.First(); k != nil; k, v = c.Next() {
+				fmt.Printf("%d. %s\n", i, v)
+				i++
+			}
+
+			return nil
+		})
+		if err != nil {
+			fmt.Printf("Couldn't create db view\n%s\n", err)
+			os.Exit(1)
+		}
 	},
 }
