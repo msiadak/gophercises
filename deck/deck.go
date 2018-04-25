8d6e1c95cb528c9f1f828d88eebe381cd08a5563
Michael Siadak <mike.siadak@gmail.com>
2018-04-25 09:14:44 -0500

    deck: Improve documentation and add SortBy()

--- deck/deck.go
+++ deck/deck.go
@@ -1,7 +1,8 @@
 package deck
 
-type Suit int
+import "sort"
 
+// Possible suits in a standard 52-card deck.
 const (
 	Spades Suit = iota
 	Diamonds
@@ -9,10 +10,9 @@ const (
 	Hearts
 )
 
-type Value int
-
+// Possible ranks in a standard 52-card deck.
 const (
-	Ace Value = iota + 1
+	Ace Rank = iota + 1
 	Two
 	Three
 	Four
@@ -27,17 +27,29 @@ const (
 	King
 )
 
+// Suit represents a card's suit.
+type Suit int
+
+// Rank represents a card's numeric or face value.
+type Rank int
+
+// Card represents a playing card in a standard 52-card deck.
 type Card struct {
-	Suit  Suit
-	Value Value
+	Suit
+	Rank
 }
 
+// New returns a new deck (slice of cards).
+//
+// By default, the deck is returned in the order that a new deck of cards will
+// come in the package (in ascending value by suit, then by rank as shown
+// in the order of the Suit and Rank constants above).
 func New(options ...func([]Card)) []Card {
 	cards := make([]Card, 52)
 
 	for i := 0; i < 4; i++ {
 		for j := 1; j <= 13; j++ {
-			cards = append(cards, Card{Suit(i), Value(j)})
+			cards = append(cards, Card{Suit(i), Rank(j)})
 		}
 	}
 
@@ -48,4 +60,31 @@ func New(options ...func([]Card)) []Card {
 	return cards
 }
 
-func Sort([]Card
+type cardSorter struct {
+	cards []Card
+	by    func(c1, c2 *Card) bool
+}
+
+func (s *cardSorter) Len() int {
+	return len(s.cards)
+}
+
+func (s *cardSorter) Swap(i, j int) {
+	s.cards[i], s.cards[j] = s.cards[j], s.cards[i]
+}
+
+func (s *cardSorter) Less(i, j int) bool {
+	return s.by(&s.cards[i], &s.cards[j])
+}
+
+// SortBy accepts a "by" function (should return true if c1 should be sorted
+// before c2) and returns a closure that operates on a slice of cards.
+//
+// The returned closure will sort the deck of cards based on the provided "by"
+// function and can be passed to New() as an option to sort the new deck.
+func SortBy(by func(c1, c2 *Card) bool) func([]Card) {
+	return func(cards []Card) {
+		sort.Sort(&cardSorter{cards, by})
+	}
+}
+
