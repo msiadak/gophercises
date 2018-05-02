7fc77b0301f3bc2ff04c5e1f714d8daa48e5a91a
Michael Siadak <mike.siadak@gmail.com>
2018-05-02 15:15:47 -0500

    quiet_hn: Refactor

--- quiet_hn/main.go
+++ quiet_hn/main.go
@@ -19,76 +19,83 @@ func main() {
 	var port, numStories, workers int
 	flag.IntVar(&port, "port", 3000, "the port to start the web server on")
 	flag.IntVar(&numStories, "num_stories", 30, "the number of top stories to display")
-	flag.IntVar(&workers, "workers", 4, "number of workers to fetch items with")
+	flag.IntVar(&workers, "workers", 8, "number of workers to fetch items with")
 	flag.Parse()
 
 	tpl := template.Must(template.ParseFiles("./index.gohtml"))
 
-	http.HandleFunc("/", handler(numStories, tpl, workers))
+	http.HandleFunc("/", handler(numStories, workers, tpl))
 
 	// Start the server
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
 }
 
-func handler(numStories int, tpl *template.Template, workers int) http.HandlerFunc {
-	totalStories := numStories + workers
+func storyFetcherPool(client hn.Client, ids []int, numWorkers int) (<-chan item, chan<- struct{}) {
+	idCh := make(chan int)
+	storyCh := make(chan item)
+	done := make(chan struct{})
+
+	go func() {
+		for _, id := range ids {
+			select {
+			case <-done:
+				close(idCh)
+				return
+			case idCh <- id:
+			}
+		}
+		close(idCh)
+	}()
+
+	for i := 0; i < numWorkers; i++ {
+		go func() {
+			for {
+				id, more := <-idCh
+				if more {
+					hnItem, err := client.GetItem(id)
+					if err != nil {
+						log.Printf("Couldn't get item %d: %s\n", id, err)
+						continue
+					}
+					item := parseHNItem(hnItem)
+					if isStoryLink(item) {
+						storyCh <- item
+					}
+				} else {
+					return
+				}
+			}
+		}()
+	}
+
+	return storyCh, done
+}
+
+func retrieveStories(n int, storyCh <-chan item, done chan<- struct{}) []item {
+	stories := make([]item, n)
+	for i := 0; i < n; i++ {
+		stories[i] = <-storyCh
+	}
+	done <- struct{}{}
+	close(done)
+	return stories
+}
+
+func handler(numStories int, numWorkers int, tpl *template.Template) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("In handler")
 		start := time.Now()
 		var client hn.Client
+
 		ids, err := client.TopItems()
 		if err != nil {
 			http.Error(w, "Failed to load top stories", http.StatusInternalServerError)
-			fmt.Println("Failed to load top stories")
 			return
 		}
 
-		idCh := make(chan int)
-		go func() {
-			for _, id := range ids {
-				idCh <- id
-			}
-			close(idCh)
-		}()
+		storyCh, done := storyFetcherPool(client, ids, numWorkers)
+		stories := retrieveStories(numStories+(numStories/5), storyCh, done)
+		sort.Sort(byDescID(stories))
 
-		storiesCh := make(chan item, workers)
-		stories := make([]item, totalStories)
-		done := make(chan struct{})
-
-		go func() {
-			for i := 0; i < totalStories; i++ {
-				stories[i] = <-storiesCh
-			}
-			sort.Sort(byDescID(stories))
-			for i := 0; i < workers+1; i++ {
-				done <- struct{}{}
-			}
-			close(done)
-		}()
-
-		for i := 0; i < workers; i++ {
-			go func(n int) {
-				for {
-					select {
-					case <-done:
-						return
-					case id := <-idCh:
-						hnItem, err := client.GetItem(id)
-						if err != nil {
-							return
-						}
-
-						item := parseHNItem(hnItem)
-						if isStoryLink(item) {
-							storiesCh <- item
-						}
-					}
-				}
-			}(i)
-		}
-
-		<-done
-		fmt.Println("executing template?")
 		data := templateData{
 			Stories: stories[:numStories],
 			Time:    time.Now().Sub(start),
